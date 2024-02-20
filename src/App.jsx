09139d5baedfde65a66c33335c69089b1d383918
[Frank Yournet] <[frankYournet@gmail.com]>
2024-02-20 14:44:33 -0500

    Created first react project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx";
+import Food from "./Food.jsx";
+
+function App() {
+  return(
+  
+  <>
+    <Footer></Footer>
+    <Header/>
+    <Food></Food>
+    <Food></Food>
+    <Food></Food>
+    <Food></Food>
+    <Food></Food>
+    
+  </>
+  
+  );
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Your website name</p>
+            <hr />
+        </footer>
+    );   
+}
+
+export default Footer;
